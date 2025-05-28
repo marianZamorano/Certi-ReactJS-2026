a7bc694e6b7a49e30413d9c79edffa86b989826c
Paul Landaeta <paulwilkerlf@gmail.com>
2025-05-28 17:26:46 -0400

    feat: login page

--- lectures/trello/src/pages/Login.tsx
+++ lectures/trello/src/pages/Login.tsx
@@ -1,5 +1,47 @@
+import { AccountCircle } from "@mui/icons-material";
+
+import { Box, CardContent, InputAdornment, TextField } from "@mui/material";
+import Container from "@mui/material/Container";
+import PasswordIcon from "@mui/icons-material/Password";
+
 function LoginPage() {
-  return <h1>Login</h1>;
+  return (
+    <Container maxWidth="xs">
+      <Box sx={{}}>
+        <CardContent>
+          <TextField
+            id="input-email-textfield"
+            label="Email"
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <AccountCircle />
+                  </InputAdornment>
+                ),
+              },
+            }}
+            variant="standard"
+          />
+
+          <TextField
+            id="input-password-textfield"
+            label="Password"
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <PasswordIcon />
+                  </InputAdornment>
+                ),
+              },
+            }}
+            variant="standard"
+          />
+        </CardContent>
+      </Box>
+    </Container>
+  );
 }
 
 export default LoginPage;
